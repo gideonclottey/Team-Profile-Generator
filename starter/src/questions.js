6172515429b0d9c17e6072099485aba5b1ff5d1c
gideonclottey <gideonclotteyint@gmail.com>
2023-02-27 23:58:37 +0000

    Creating classes and init function

---
+++ starter/src/questions.js
@@ -0,0 +1,74 @@
+// manager questions
+const manager = [
+    {
+      type: 'input',
+      name: 'name',
+      message: "What is Manger's first name?",
+    },
+    {
+      type: 'number',
+      name: 'id',
+      message: 'What is their id number?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is their email?',
+    },
+    {
+      type: 'number',
+      name: 'office',
+      message: 'What is their Office number?'
+    }
+  
+  ];
+  
+  // engineer questions
+  const engineer = [
+    {
+      type: 'input',
+      name: 'name',
+      message: "What is Engineer's first name?",
+    },
+    {
+      type: 'number',
+      name: 'id',
+      message: 'What is their id number?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is their email?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is their Github user name?'
+    }
+  
+  ];
+  
+  // intern questions
+  const intern = [
+    {
+      type: 'input',
+      name: 'name',
+      message: "What is Intern's first name?",
+    },
+    {
+      type: 'number',
+      name: 'id',
+      message: 'What is their id number?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is their email?',
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: 'What school do they attend?'
+    }
+  ];
+  
